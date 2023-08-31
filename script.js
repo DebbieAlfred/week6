df3704ac234d9a8d5ce8ff643dc283749be95d90
Etini Alphonsus Alfred <etinialfred@gmail.com>
2023-08-31 23:02:12 +0100

    create a function

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict"
+
+/*You are presented with a string with a collection of data which are mixed alphabets i.e., "abdsgyhidsaadgbdaasbvcydsaabdgsaayre", create a function that will count through the string and show the number of a, b, d and y each contained in the string.
+Note: the output is expected to be a number for each of the counted elements.*/ 
+function alphabets(str){
