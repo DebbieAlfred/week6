621d30a88dd11a151fe3d3b2b49edc5de981f06e
Etini Alphonsus Alfred <etinialfred@gmail.com>
2023-08-31 23:05:47 +0100

    create empty container

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 /*You are presented with a string with a collection of data which are mixed alphabets i.e., "abdsgyhidsaadgbdaasbvcydsaabdgsaayre", create a function that will count through the string and show the number of a, b, d and y each contained in the string.
 Note: the output is expected to be a number for each of the counted elements.*/ 
 function alphabets(str){
+let strArray=str.split("");
+let containerA = [];
+let containerB = [];
+let containerD = [];
+let containerY = [];
